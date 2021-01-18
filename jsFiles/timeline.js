d92fdd6b4bfb3d3019854b4bd5ab2b236a956e62
apeirophilia <davidemelnikoff@gmail.com>
2021-01-18 15:09:14 -0500

    update

---
+++ jsFiles/timeline.js
@@ -0,0 +1,7 @@
+var trial = {
+    type: 'canvas-keyboard-response',
+    stimulus: drawRect,
+    choices: ['e','i'],
+    prompt: '<p>Is this a circle or a rectangle? Press "e" for circle and "i" for rectangle.</p>',
+    data: {shape: 'rectangle'}
+}
