db00ef451f96c6b2838ffd29508536c44382b3d7
apeirophilia <davidemelnikoff@gmail.com>
2021-01-18 15:11:20 -0500

    update

--- jsFiles/game.js
+++ jsFiles/game.js
@@ -1,21 +1,5 @@
-function create() {
-  // Change "Codey's Adventures\n  in Code World" to the name of your game
-  this.add.text(50, 100, "Flow as Mutual Information", { font: "40px Times New Roman", fill: "#ffa0d0"});
-
-  // Change "by Codecademy" to your name!
-  this.add.text(130, 300, "by David E. Melnikoff", { font: "20px Times New Roman", fill: "#ffa0d0"});
+function draw(c) {
+	const ctx = c.getContext('2d');
+	ctx.fillStyle = 'red';
+	ctx.fillRect(20, 20, 150, 100);
 }
-
-const config = {
-	type: Phaser.AUTO,
-	width: 600,
-	height: 600,
-	backgroundColor: "#5f2a55",
-	scene: {
-    create
-	}
-};
-
-const game = new Phaser.Game(config);
-
-console.log('check');
