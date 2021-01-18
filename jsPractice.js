107881b46b181d29ba94b92715804720f2f153e1
apeirophilia <davidemelnikoff@gmail.com>
2021-01-18 10:47:45 -0500

    update

--- jsPractice.js
+++ jsPractice.js
@@ -3,7 +3,7 @@ function create() {
   this.add.text(50, 100, "Flow as Mutual Information", { font: "40px Times New Roman", fill: "#ffa0d0"});
 
   // Change "by Codecademy" to your name!
-  this.add.text(130, 300, "by David Melnikoff", { font: "20px Times New Roman", fill: "#ffa0d0"});
+  this.add.text(130, 300, "by David E. Melnikoff", { font: "20px Times New Roman", fill: "#ffa0d0"});
 }
 
 const config = {
